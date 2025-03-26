143d3120fa5e56ffffce6922a44a2086dcc005d4
masanitycom <masataka.tak@gmail.com>
2025-03-26 17:54:02 +0900

    components/theme-provider.tsxファイルを修正

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,12 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import type * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+
+type ThemeProviderProps = React.PropsWithChildren<NextThemeProviderProps>
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
