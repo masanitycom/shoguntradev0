36424b2988dcbff61583922dd2be3d2316b55bbe
masanitycom <masataka.tak@gmail.com>
2025-03-26 17:56:08 +0900

    components/theme-provider.tsxファイルを修正

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,9 +2,14 @@
 
 import type * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
 
-type ThemeProviderProps = React.PropsWithChildren<NextThemeProviderProps>
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
